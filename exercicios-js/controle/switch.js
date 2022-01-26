409bdf7a04c55f91f17c37d78ff2d11445ac3e74
Marcelo Antonio Pereira Marcolino <marceloapmarcolino@bol.com.br>
2022-01-25 23:16:32 -0300

    WHile

--- exercicios-js/controle/switch.js
+++ exercicios-js/controle/switch.js
@@ -3,5 +3,25 @@ const imprimirResultado2 = function (nota) {
         case 10:
         case 9:
             console.log('Quadro de Honra')
+            break
+        case 8: case 7:
+            console.log('Aprovado')
+            break
+        case 6: case 5: case 4:
+            console.log('Recuperação')
+            break
+        case 3: case 2: case 1: case 0:
+            console.log('Reprovado')
+            break
+        default:
+            console.log('Nota inválida')
     }
+    console.log('Fim!')
 }
+
+imprimirResultado2(10)
+imprimirResultado2(8.9)
+imprimirResultado2(6.55)
+imprimirResultado2(2.3)
+imprimirResultado2(-1)
+imprimirResultado2(11)
